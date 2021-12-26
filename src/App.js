c361e2340f46f864f0c88e007b0f4133b798ebb2
nedal <nedal@zetaton.com>
2021-12-26 16:20:52 +0200

    yarn configration

--- src/App.js
+++ src/App.js
@@ -1,47 +1,48 @@
-import { useEffect } from 'react';
-import './App.css';
-import Home from './components/Home';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Explore from './components/Explore';
-import Navbar from './components/Navbar';
-import ShareForm from './components/ShareForm';
-import Registration from './components/Registration';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import ProfilePage from './components/ProfilePage';
+import { useEffect } from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Explore from "./components/Explore";
+import Navbar from "./components/Navbar";
+import ShareForm from "./components/ShareForm";
+import Registration from "./components/Registration";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import ProfilePage from "./components/ProfilePage";
 
-import { Report } from 'notiflix/build/notiflix-report-aio';
-import UserAdditionDataForm from './components/UserAdditionDataForm';
+import { Report } from "notiflix/build/notiflix-report-aio";
+import UserAdditionDataForm from "./components/UserAdditionDataForm";
 function App() {
-	useEffect(() => {
-		Report.info(
-			'Why Wavy',
-			'The idea comes when we thougt that adventurers have kind of unstable life so we came up with wavy name',
-			'Got it'
-		);
-	}, []);
-	return (
-		<Router>
-			<div className=''>
-				<Navbar />
-				<Switch>
-					<Route exact path='/' component={Home} />
+  useEffect(() => {
+    Report.info(
+      "Why Wavy",
+      "The idea comes when we thougt that adventurers have kind of unstable life so we came up with wavy name",
+      "Got it"
+    );
+  }, []);
+  return (
+    <Router>
+      <div className="">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
 
-					<Route exact path='/explore' component={Explore} />
-					<Route exact path='/share' component={ShareForm} />
-					<Route exact path='/registration' component={Registration} />
-					<Route exact path='/login' component={Login} />
-					<Route exact path='/profile' component={ProfilePage} />
-					<Route
-						exact
-						path='/additional_data_form'
-						component={UserAdditionDataForm}
-					/>
-				</Switch>
-				<Footer />
-			</div>
-		</Router>
-	);
+          <Route exact path="/explore" component={Explore} />
+          <Route exact path="/share" component={ShareForm} />
+          <Route exact path="/profile-page" component={ProfilePage} />
+          <Route exact path="/registration" component={Registration} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route
+            exact
+            path="/additional_data_form"
+            component={UserAdditionDataForm}
+          />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
