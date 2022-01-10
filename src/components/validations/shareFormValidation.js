aaf5516fed1ec87290cb800605f65530073325cd
nedal <nedal@zetaton.com>
2022-01-10 20:09:45 +0200

    resolve conflict 10/1/2022

--- src/components/validations/shareFormValidation.js
+++ src/components/validations/shareFormValidation.js
@@ -1,7 +1,6 @@
 import * as yup from "yup";
 
 export const shareFormValidation = yup.object().shape({
-<<<<<<< HEAD
   title: yup
     .string()
     .required("Please enter a title")
@@ -12,15 +11,4 @@ export const shareFormValidation = yup.object().shape({
     .required("Please enter a description")
     .min(10, "Description must be at least 10 characters")
     .max(100, "Description must be at most 100 characters"),
-=======
-	title: yup
-		.string()
-		.required('Please enter a title')
-		.min(2, 'Title must be at least 2 characters'),
-	description: yup
-		.string()
-		.required('Please enter a description')
-		.min(10, 'Description must be at least 10 characters')
-		.max(100, 'Description must be at most 100 characters'),
->>>>>>> bfd2d9fd739677b0128846a030667a1467aaac8f
 });
